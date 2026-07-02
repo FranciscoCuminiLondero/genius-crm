700d90a8621762e0488ec1a79c2b436e0148b8ea
Francisco Cumini Londero <cuminifrancisco@gmail.com>
2026-07-01 21:21:02 -0300

    fix: corregir formato de importación de cors en index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors') 
+const cors = require('cors')
 const swaggerJsdoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
 const templateRoutes = require('./routes/templates')
